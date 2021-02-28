56867ddb01f21e1d2fd4c4b5887e63a118d82192
DAG <acostagdiego@gmail.com>
2021-02-28 16:48:49 -0300

    Stage 2 complete

--- TextBasedAdventureGame.py
+++ TextBasedAdventureGame.py
@@ -6,11 +6,47 @@ def print_mainmenu():
     "3- Press key '3' or type 'quit' to quit the game",
     sep="\n")
 
+def create_new_user(username, database):
+    database[username] = {'name': input("1- Name ").capitalize(), 
+                          'species': input("2- Species ").capitalize(),
+                          'gender': input("3- Gender ").capitalize(),
+                          'snack': input("1- Favourite Snack ").capitalize(),
+                          'weapon': input("2- A weapon for the journey ").capitalize(),
+                          'tool': input("3- A traversal tool ").capitalize()}
+    
+    while True:
+        difficulty = input("Choose your difficulty:\n1- Easy\n2- Medium\n3- Hard\n").casefold()
+        if difficulty == '1' or difficulty == 'easy':
+            database[username]['difficulty'] = 'Easy'
+            break
+        elif difficulty == '2' or difficulty == 'medium':
+            database[username]['difficulty'] = 'Medium'
+            break
+        elif difficulty == '3' or difficulty == 'hard':
+            database[username]['difficulty'] = 'Hard'
+            break
+        print("Invalid input.")
+
+    return database
+    
+user_database = {}
+
 while True:
     print_mainmenu()
     selection = input().casefold()
     if selection == "1" or selection == "start":
         print("Starting a new game...")
+        username = input("Enter a user name to save your progress or type '/b' to go back ")
+        if username == '/b':
+            print("Going back to menu...\n")
+            continue
+        else:
+            user_database = create_new_user(username, user_database)
+            print("Good luck on your journey!")
+            print(f"Your character: {user_database[username]['name']}, {user_database[username]['species']}, {user_database[username]['gender']}")
+            print(f"Your inventory: {user_database[username]['snack']}, {user_database[username]['weapon']}, {user_database[username]['tool']}")
+            print(f"Difficulty: {user_database[username]['difficulty']}")
+            print()
     elif selection == "2" or selection == "load":
         print("No save data found!")
     elif selection == "3" or selection == "quit":
